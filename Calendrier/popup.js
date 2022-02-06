d2fbc833699a34351ff91bab494caa2597da6bdd
Romanemoison <77722764+Romanemoison@users.noreply.github.com>
2022-02-06 15:44:16 +0100

    popups

--- Calendrier/popup.js
+++ Calendrier/popup.js
@@ -1,21 +1,29 @@
-// Get the modal
-var modal = document.getElementById('myModal');
+function popup(IdMosal, IdImg,IdModalImg, IdCaption, ClasseSpan){  
+  // alert(IdMosal);
+  // alert(IdImg);
+  // alert(IdModalImg);
+  // alert(IdCaption);
+  // alert(ClasseSpan);
 
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById("myImg");
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
-bouton1.onclick = function(){
-  modal.style.display = "block";
-  modalImg.src = img.src;
-  captionText.innerHTML = img.alt;
-}
+  // Get the modal
+  var modal = document.getElementById(IdMosal);
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
+  // Get the image and insert it inside the modal - use its "alt" text as a caption
+  var img = document.getElementById(IdImg);
+  var modalImg = document.getElementById(IdModalImg);
+  var captionText = document.getElementById(IdCaption);
+  
+    modal.style.display = "block";
+    modalImg.src = img.src;
+    captionText.innerHTML = img.alt;
+  
+  // alert(modal.style.display);
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName(ClasseSpan)[0];
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+    modal.style.display = "none";
+    // alert('in '+ modal.style.display);
+  }
 }
 
